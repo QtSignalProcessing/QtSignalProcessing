9e11d8f0687a18c7d1d0d2ff224ad3cb3e81ce9e
le <merkur07>
2013-11-10 18:19:49 +0100

    delete unused headers

--- main.cpp
+++ main.cpp
@@ -1,26 +1,7 @@
 #include <QtGui/QApplication>
+
 #include "mainwindow.h"
-#include"glwidget.h"
-#include"plot.h"
-#include"plotospectrum.h"
-#include"plotsw.h"
-#include"glsw.h"
-#include<iostream>
-#include<QDoubleSpinBox>
-#include<QGridLayout>
-#include <QDebug>
-#include <phonon/audiooutput.h>
-#include <phonon/mediaobject.h>
-#include <phonon/mediasource.h>
-#include<math.h>
-#include"iir.h"
-#include"xwilkinson.h"
 
-
-
-using namespace Phonon;
-
-using namespace std;
 int main(int argc, char *argv[])
 {
    QApplication a(argc, argv);
